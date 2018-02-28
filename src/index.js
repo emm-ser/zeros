cb3f743b596d78d9ccafb0edd06a9c76f77f408e
emm-ser <106416@bk.ru>
2018-02-28 22:28:42 +0300

    zeros/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    
+    var zeros = 0;
+
+    var i = 5;
+
+    while (true) {
+        var temp = Math.floor(number / i);
+
+        if (temp >= 1) {
+            zeros += temp;
+            i *= 5;
+        } else {
+            break;
+        }
+    }
+
+    return zeros;
 }
